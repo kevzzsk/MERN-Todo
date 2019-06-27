ccd6f9dc2bbcbe32de9ff6e9be901369f8af6d51
KevinSk <kevi0023@e.ntu.edu.sg>
2019-06-27 15:24:52 +0800

    Add Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import { Container } from 'reactstrap';
 
-import'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
